16a0eb71a3af3d049b08d7f67f0390964fef5545
yukito-h <yukito1009ttp@gmail.com>
2021-01-06 23:03:27 +0900

    1.8
    update２

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ const hDate = document.getElementById('header_date')
 const samary = document.querySelector(".samary");
 const main = document.querySelector('main');
 
+//1set 追加する関数
 function addSamary() {
   const main2 =samary.cloneNode(true);
   main.appendChild(main2);
-
-
 }
 
-addSamary();
-addSamary();
+
+
+// addSamary();
